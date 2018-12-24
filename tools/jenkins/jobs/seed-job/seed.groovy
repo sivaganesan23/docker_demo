bab19b825007be45328b51acb36de9dc6752148a
Raghu K <raghuk.vit@gmail.com>
2018-12-24 02:46:27 +0000

    Updating seed-job

---
+++ tools/jenkins/jobs/seed-job/seed.groovy
@@ -0,0 +1,32 @@
+pipelineJob('dev-pipeline-job') {
+  description('Dev-PipeLine-Job')
+  displayName('Dev-PipeLine-Job')
+  configure { flowdefinition ->
+    flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
+      'triggers' {
+        'hudson.triggers.SCMTrigger' {
+          'spec'('H/2 * * * *')
+          'ignorePostCommitHooks'(false)
+        }
+      }
+    }
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/citb33/dockers.git')
+          }
+        }
+
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+      }
+      'scriptPath'('tools/jenkins/jobs/pipeline-jobs/MAIN-dev-pipeline-job.groovy')
+      'lightweight'(true)
+    }
+  }
+}
+
