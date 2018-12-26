c18f0dd63fe15343ce537a779ff9c4aac166189c
Raghu K <raghuk.vit@gmail.com>
2018-12-26 02:09:15 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/MAIN-dev-pipeline-job.groovy
+++ tools/jenkins/jobs/pipeline-jobs/MAIN-dev-pipeline-job.groovy
@@ -6,4 +6,8 @@ node('SLAVE') {
   stage('COMPILE'){
      sh 'mvn compile'
    }
+
+  stage('Code Quality Check') {
+    sh 'mvn sonar:sonar -Dsonar.host.url=http://52.15.75.213:9000 -Dsonar.login=264927da4ae2e638337076b232cd2e1687bdca28'
+  }
 }
