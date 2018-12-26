3502f6baa03099d7080afb7772f2ebad501b8ab7
Raghu K <raghuk.vit@gmail.com>
2018-12-26 02:40:36 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/MAIN-dev-pipeline-job.groovy
+++ tools/jenkins/jobs/pipeline-jobs/MAIN-dev-pipeline-job.groovy
@@ -10,4 +10,8 @@ node('SLAVE') {
   stage('Code Quality Check') {
     sh 'mvn sonar:sonar -Dsonar.projectKey=citb33_studentproj-code -Dsonar.organization=citb33 -Dsonar.host.url=https://sonarcloud.io   -Dsonar.login=24dc0afa2658cdc57cdce6f5d3d7538f3ded4276'
   }
+
+  stage('Packaging'){
+    sh 'mvn package'
+  }
 }
