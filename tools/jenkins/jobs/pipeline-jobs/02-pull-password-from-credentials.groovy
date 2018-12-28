af122c749bdba257de38bba7c32d050567d40750
Raghu K <raghuk.vit@gmail.com>
2018-12-28 02:47:11 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/02-pull-password-from-credentials.groovy
+++ tools/jenkins/jobs/pipeline-jobs/02-pull-password-from-credentials.groovy
@@ -1,7 +1,7 @@
 node('SLAVE') {
     stage('Variables') {
         withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'SAMPLE-USER-PASS', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-            sh 'echo uname=$USERNAME pwd=$PASSWORD'
+            sh 'echo uname=$USERNAME pwd=$PASSWORD >/tmp/abc'
         }
     }
 }
