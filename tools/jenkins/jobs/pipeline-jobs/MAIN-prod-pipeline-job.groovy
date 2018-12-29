306edc70a61c5c0622e27baf7ca6b5236287c1a8
Raghu K <raghuk.vit@gmail.com>
2018-12-29 03:12:37 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
+++ tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
@@ -1,33 +1,9 @@
 node('SLAVE') {
-  stage('CLONE') {
-    dir('APPCODE') {
-      git 'https://github.com/citb33/studentproj-code.git' 
-    }
-   }
-
-  stage('COMPILE'){
-    dir('APPCODE') {
-     sh 'mvn compile -D VERSION=$VERSIONNO -D TYPE=$VERSIONTYPE'
-    }
-   }
-
-  stage('Code Quality Check') {
-    dir('APPCODE') {
-      sh 'mvn sonar:sonar -Dsonar.projectKey=citb33_studentproj-code -Dsonar.organization=citb33 -Dsonar.host.url=https://sonarcloud.io   -Dsonar.login=24dc0afa2658cdc57cdce6f5d3d7538f3ded4276 -D VERSION=$VERSIONNO -D TYPE=$VERSIONTYPE'
-    }
-  }
-
-  stage('Packaging'){
-    dir('APPCODE') {
-      sh 'mvn package -D VERSION=$VERSIONNO -D TYPE=$VERSIONTYPE'
-    }
-  }
-
   stage('TEST-ENV-CREATE') {
     dir('TERRAFORM') {
       git 'https://github.com/citb33/terraform.git'
+      wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
       withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'TERRAFORM-KEYS', usernameVariable: 'ACCESS_KEY', passwordVariable: 'SECRET_KEY']]) {
-        wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
           sh '''
 export AWS_ACCESS_KEY_ID=$ACCESS_KEY
 export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
