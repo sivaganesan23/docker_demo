57f364cf301c51fd2ce833c616bc5fc6214b0732
Raghu K <raghuk.vit@gmail.com>
2018-12-29 03:00:24 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
+++ tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
@@ -26,12 +26,16 @@ node('SLAVE') {
   stage('TEST-ENV-CREATE') {
     dir('TERRAFORM') {
       git 'https://github.com/citb33/terraform.git'
+      withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'TERRAFORM-KEYS', usernameVariable: 'ACCESS_KEY', passwordVariable: 'SECRET_KEY']]) {
       sh '''
+export AWS_ACCESS_KEY_ID=$ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
+export AWS_DEFAULT_REGION=us-east-2
 cd stack-test-env
 terraform init
 terraform apply -auto-approve
 '''
+     }
     }
-
   }
 }
