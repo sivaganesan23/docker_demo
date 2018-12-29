24432cd967bcde490f9ab2b4768d5d1074aaf15e
Raghu K <raghuk.vit@gmail.com>
2018-12-29 02:37:41 +0000

    Updating docker

--- tools/jenkins/jobs/seed-job/seed.groovy
+++ tools/jenkins/jobs/seed-job/seed.groovy
@@ -30,3 +30,31 @@ pipelineJob('dev-pipeline-job') {
   }
 }
 
+
+pipelineJob('prod-pipeline-job') {
+  description('Prod-PipeLine-Job')
+  displayName('Prod-PipeLine-Job')
+  parameters {
+    stringParam('VERSIONNO', 'NA', 'Version Number of Application')
+    stringParam('VERSIONTYPE', 'RELEASE', 'Version Type is Release')
+  }
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/citb33/docker.git')
+          }
+        }
+
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/master')
+          }
+        }
+      }
+      'scriptPath'('tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy')
+      'lightweight'(true)
+    }
+  }
+}
