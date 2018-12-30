4127408266c876fac42232e02721eddc4edca565
Raghu K <raghuk.vit@gmail.com>
2018-12-30 03:06:41 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
+++ tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
@@ -55,7 +55,7 @@ chmod 600 /home/centos/devops.pem
         ansible-playbook -i /tmp/hosts deploy.yml
         '''
       }
-    } finally {
+    } catch(Exception ex) {
       sh 'rm -f /home/centos/devops.pem'
     } 
   }
@@ -73,20 +73,24 @@ chmod 600 /home/centos/devops.pem
   }
 
   stage('Clean-Test-Env') {
-    dir('TERRAFORM') {
-            git 'https://github.com/citb33/terraform.git'
-            wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
-            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'TERRAFORM-KEYS', usernameVariable: 'ACCESS_KEY', passwordVariable: 'SECRET_KEY']]) {
-                sh '''
-      export AWS_ACCESS_KEY_ID=$ACCESS_KEY
-      export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
-      export AWS_DEFAULT_REGION=us-east-2
-      cd stack-test-env
-      terraform init
-      terraform destroy -auto-approve
-      '''
-            }
-        }
+    try {
+      dir('TERRAFORM') {
+              git 'https://github.com/citb33/terraform.git'
+              wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+              withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'TERRAFORM-KEYS', usernameVariable: 'ACCESS_KEY', passwordVariable: 'SECRET_KEY']]) {
+                  sh '''
+        export AWS_ACCESS_KEY_ID=$ACCESS_KEY
+        export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
+        export AWS_DEFAULT_REGION=us-east-2
+        cd stack-test-env
+        terraform init
+        terraform destroy -auto-approve
+        '''
+              }
+          }
+      }
+    } finally {
+      sh 'rm -f /home/centos/devops.pem'
     }
   }
 }
