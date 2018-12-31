cbc218ba6614e8fe9477300291b624b03680229a
Raghu K <raghuk.vit@gmail.com>
2018-12-31 03:05:53 +0000

    Updating docker

--- tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
+++ tools/jenkins/jobs/pipeline-jobs/MAIN-prod-pipeline-job.groovy
@@ -79,5 +79,25 @@ chmod 600 /home/centos/devops.pem
     echo 'Upload-Artifacts'
   }
 
-
+  stage('Clean-Test-Env') {
+    try {
+      dir('TERRAFORM') {
+              git 'https://github.com/citb33/terraform.git'
+              wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'xterm']) {
+              withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'TERRAFORM-KEYS', usernameVariable: 'ACCESS_KEY', passwordVariable: 'SECRET_KEY']]) {
+                  sh '''
+        export AWS_ACCESS_KEY_ID=$ACCESS_KEY
+        export AWS_SECRET_ACCESS_KEY=$SECRET_KEY
+        export AWS_DEFAULT_REGION=us-east-2
+        cd stack-test-env
+        terraform init
+        terraform destroy -auto-approve
+        '''
+              }
+          }
+      }
+    } finally {
+      sh 'rm -f /home/centos/devops.pem'
+    }
+  }
 }
